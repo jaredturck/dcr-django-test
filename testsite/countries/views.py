18095d0d9105faa6790c18cd17ac2c5910d7e5b4
Jared Turck <jaredturck8@gmail.com>
2025-07-24 13:31:45 +0100

    Completed Exercise 1 - Complete Stats View

--- testsite/countries/views.py
+++ testsite/countries/views.py
@@ -1,8 +1,13 @@
 from django.http import JsonResponse
-
+from countries.models import Region
+from django.db.models import Count, Sum, F
 
 def stats(request):
-    # TODO - Provide name, number_countries and total_population for each region
-    response = {"regions": []}
+    ''' Get a count of total population, and number of countries in each region '''
+ 
+    qs = Region.objects.all().annotate(
+        number_countries = Count(F('countries__name')),
+        total_population = Sum(F('countries__population'))
+    ).values('name', 'number_countries', 'total_population')
 
-    return JsonResponse(response)
+    return JsonResponse({"regions": list(qs)})
